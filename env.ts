70d72ef00ff365b6e86d40ae3839d4bed1ca640b
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-08 16:32:24 -0500

    feat: Add env variable runtime validation with zod

---
+++ env.ts
@@ -0,0 +1,26 @@
+import { ZodError, z } from "zod";
+
+export const envSchema = z.object({
+	NODE_ENV: z.enum(["development", "production"]).default("development"),
+	//! TODO: Add more env variables here
+});
+
+type EnvConfig = z.infer<typeof envSchema>;
+
+const validateEnvConfig = (): EnvConfig => {
+	try {
+		return envSchema.parse(process.env);
+	} catch (error) {
+		if (error instanceof ZodError) {
+			const errorMessage = Object.entries(error.flatten().fieldErrors)
+				.map(([field, errors]) => {
+					return `${field}: ${errors?.join(", ")}`;
+				})
+				.join("\n");
+			throw new Error(`Invalid environment variables:\n${errorMessage}`);
+		}
+		throw error;
+	}
+};
+
+export const envConfig = validateEnvConfig();
