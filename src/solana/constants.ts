7945bdb5f393ea58cdaa39ed43167e28cf7dbd12
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-08 16:46:03 -0500

    feat: Add solana base58 address regex constant

--- src/solana/constants.ts
+++ src/solana/constants.ts
@@ -7,3 +7,6 @@ export const BIRDEYE_BASE_HEADERS = {
 	"X-API-Key": envConfig.BIRDEYE_API_KEY,
 	"x-chain": "solana",
 } as const;
+
+//* Solana
+export const SOLANA_BASE58_ADDRESS_REGEX = /^[1-9A-HJ-NP-Za-km-z]{32,44}$/;
