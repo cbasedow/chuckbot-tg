6bc6c34db1ae0189bac386bd400c20bd5405bdcc
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-08 16:57:08 -0500

    feat: Add Birdeye token creation info schema
    - Add token creation info schema and type
    - Add token creation info response schema

--- src/solana/schemas/birdeye.ts
+++ src/solana/schemas/birdeye.ts
@@ -6,3 +6,11 @@ export const birdeyeApiResponseSchema = <T extends z.ZodTypeAny>(responseSchema:
 		data: responseSchema.nullish(),
 	});
 };
+
+export const tokenCreationInfoSchema = z.object({
+	txHash: z.string().min(1),
+});
+
+export const tokenCreationInfoResponseSchema = birdeyeApiResponseSchema(tokenCreationInfoSchema);
+
+export type TokenCreationInfo = z.infer<typeof tokenCreationInfoSchema>;
