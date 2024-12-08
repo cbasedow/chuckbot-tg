1d10844bd6994817b50048a891048b49f8b1b826
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-08 17:18:53 -0500

    feat: Add Helius Parse Transactions API constants

--- src/solana/constants.ts
+++ src/solana/constants.ts
@@ -15,5 +15,12 @@ export const HELIUS_RPC_BASE_REQUEST = {
 	id: crypto.randomUUID(),
 };
 
+//* Helius Parse Transactions API
+export const HELIUS_PARSE_TXNS_BASE_URL = "https://api.helius.xyz/v0/transactions";
+export const HELIUS_PARSE_TXNS_DEFAULT_PARAMS = {
+	"api-key": envConfig.HELIUS_API_KEY,
+	commitment: "confirmed",
+} as const;
+
 //* Solana
 export const SOLANA_BASE58_ADDRESS_REGEX = /^[1-9A-HJ-NP-Za-km-z]{32,44}$/;
