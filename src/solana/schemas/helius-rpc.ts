8279a3e4143778797468cac36968b9d184038204
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-08 22:06:59 -0500

    feat: Add Helius RPC Das Asset schema
    - Add metadata, link, content and token info schemas
    - Add main Das Asset schema and type
    - Add Das Asset response schema

--- src/solana/schemas/helius-rpc.ts
+++ src/solana/schemas/helius-rpc.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { solanaBase58AddressSchema } from "./solana";
 
 export const heliusRpcResponseSchema = <T extends z.ZodTypeAny>(resultSchema: T) => {
 	return z.object({
@@ -7,3 +8,38 @@ export const heliusRpcResponseSchema = <T extends z.ZodTypeAny>(resultSchema: T)
 		result: resultSchema,
 	});
 };
+
+//* DAS getAsset
+const metadataSchema = z.object({
+	description: z.string().nullish(),
+	name: z.string().min(1),
+	symbol: z.string().min(1),
+});
+
+const linkSchema = z.object({
+	image: z.string().min(1).nullish(),
+});
+
+const contentSchema = z.object({
+	json_uri: z.string().url().nullish(),
+	metadata: metadataSchema,
+	links: linkSchema.nullish(),
+});
+
+const tokenInfoSchema = z.object({
+	balance: z.number().min(0).nullish(),
+	supply: z.number().min(0),
+	decimals: z.number().min(0),
+	freeze_authority: solanaBase58AddressSchema.nullish(),
+	mint_authority: solanaBase58AddressSchema.nullish(),
+});
+
+export const dasAssetSchema = z.object({
+	id: solanaBase58AddressSchema,
+	content: contentSchema,
+	mutable: z.boolean(),
+	token_info: tokenInfoSchema,
+});
+
+export const dasAssetResponseSchema = heliusRpcResponseSchema(dasAssetSchema);
+export type DasAsset = z.infer<typeof dasAssetSchema>;
