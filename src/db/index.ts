f5b5ccdf0e996c975d3feb40e5a3e6296d751c5e
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-09 15:49:53 -0500

    feat: Add Telegram db schemas
    - Add users,groups and refLinks tables, relations, zod schemas and types
    - Add schema to db client

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,9 +2,11 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import { envConfig } from "env";
 import postgres from "postgres";
 
+import * as schema from "./schema";
+
 const client = postgres(envConfig.DATABASE_URL, {
 	max: 15,
 	idle_timeout: 30, // seconds
 });
 
-export const db = drizzle({ client });
+export const db = drizzle({ client, schema });
---
+++ src/db/schema.ts
@@ -0,0 +1,103 @@
+import { relations, sql } from "drizzle-orm";
+import { bigint, pgEnum, pgTable, serial, timestamp, uniqueIndex, varchar } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+
+//* Tables
+export const users = pgTable(
+	"tg_users",
+	{
+		id: bigint("id", { mode: "number" }).primaryKey(),
+		username: varchar("username", { length: 32 }),
+		createdAt: timestamp("created_at").notNull().defaultNow(),
+	},
+	(t) => [uniqueIndex("tg_username_idx").on(t.username).where(sql`username IS NOT NULL`)],
+);
+
+export const groups = pgTable("tg_groups", {
+	id: bigint("id", { mode: "number" }).primaryKey(),
+	name: varchar("name", { length: 255 }).notNull(),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const refLinkPlatformEnum = pgEnum("ref_link_platform", [
+	"bonk",
+	"bullx",
+	"maestro",
+	"photon",
+	"shuriken",
+	"trojan",
+]);
+
+export const refLinks = pgTable(
+	"tg_group_ref_links",
+	{
+		id: serial("id").primaryKey(),
+		groupId: bigint("group_id", { mode: "number" })
+			.references(() => groups.id, { onDelete: "cascade" })
+			.notNull(),
+		platform: refLinkPlatformEnum("platform").notNull(),
+		url: varchar("url", { length: 255 }).notNull(),
+		createdAt: timestamp("created_at").notNull().defaultNow(),
+		createdBy: bigint("created_by", { mode: "number" })
+			.references(() => users.id)
+			.notNull(),
+		updatedAt: timestamp("updated_at").notNull().defaultNow(),
+		updatedBy: bigint("updated_by", { mode: "number" })
+			.references(() => users.id)
+			.notNull(),
+	},
+	(t) => [
+		uniqueIndex("tg_group_ref_platform_idx").on(t.platform, t.groupId),
+		uniqueIndex("tg_group_ref_url_idx").on(t.url, t.groupId),
+	],
+);
+
+//* Relations
+export const usersRelations = relations(users, ({ many }) => {
+	return {
+		createdRefLinks: many(refLinks, { relationName: "createdRefLinks" }),
+		updatedRefLinks: many(refLinks, { relationName: "updatedRefLinks" }),
+	};
+});
+
+export const groupsRelations = relations(groups, ({ many }) => {
+	return {
+		refLinks: many(refLinks),
+	};
+});
+
+export const refLinksRelations = relations(refLinks, ({ one }) => {
+	return {
+		group: one(groups, {
+			fields: [refLinks.groupId],
+			references: [groups.id],
+		}),
+		createdBy: one(users, {
+			fields: [refLinks.createdBy],
+			references: [users.id],
+			relationName: "createdRefLinks",
+		}),
+		updatedBy: one(users, {
+			fields: [refLinks.updatedBy],
+			references: [users.id],
+			relationName: "updatedRefLinks",
+		}),
+	};
+});
+
+//* Zod Schemas
+export const userSchema = createSelectSchema(users);
+export const groupSchema = createSelectSchema(groups);
+export const refLinkPlatformSchema = z.enum(refLinkPlatformEnum.enumValues);
+export const refLinkSchema = createSelectSchema(refLinks);
+
+export const insertUserSchema = createInsertSchema(users);
+export const insertGroupSchema = createInsertSchema(groups);
+export const insertRefLinkSchema = createInsertSchema(refLinks);
+
+//* Types
+export type User = z.infer<typeof userSchema>;
+export type Group = z.infer<typeof groupSchema>;
+export type RefLinkPlatform = z.infer<typeof refLinkPlatformSchema>;
+export type RefLink = z.infer<typeof refLinkSchema>;
