b3c94a679622a55f18388f83bd81d2c0353939f8
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-09 12:58:52 -0500

    feat: Add Solana parsed program account schema
    - Add token amount, parsed token account info, parsed token account, parsed token account data, parsed token account schemas
    - Add parsedProgramAccountsResponse schema
    - Add parsed token account info type

--- src/solana/schemas/solana.ts
+++ src/solana/schemas/solana.ts
@@ -1,4 +1,42 @@
 import { z } from "zod";
 import { SOLANA_BASE58_ADDRESS_REGEX } from "../constants";
+import { heliusRpcResponseSchema } from "./helius-rpc";
 
 export const solanaBase58AddressSchema = z.string().regex(SOLANA_BASE58_ADDRESS_REGEX);
+
+//* Solana Parsed Token Account
+export const tokenAmountSchema = z.object({
+	amount: z.string().min(1), // Raw amount
+	decimals: z.number().min(0),
+	uiAmount: z.number().min(0).nullable(), // deprecated
+	uiAmountString: z.string().min(1),
+});
+
+export const parsedTokenAccountInfoSchema = z.object({
+	mint: solanaBase58AddressSchema,
+	owner: solanaBase58AddressSchema,
+	tokenAmount: tokenAmountSchema,
+});
+
+export const parsedTokenAccountSchema = z.object({
+	info: parsedTokenAccountInfoSchema,
+	type: z.literal("account"),
+});
+
+export const parsedTokenAccountDataSchema = z.object({
+	parsed: parsedTokenAccountSchema,
+});
+
+export const parsedProgramTokenAccountSchema = z.object({
+	data: parsedTokenAccountDataSchema,
+});
+
+export const parsedProgramAccountsResponseSchema = heliusRpcResponseSchema(
+	z
+		.object({
+			account: parsedProgramTokenAccountSchema,
+		})
+		.array(),
+);
+
+export type ParsedTokenAccountInfo = z.infer<typeof parsedTokenAccountInfoSchema>;
