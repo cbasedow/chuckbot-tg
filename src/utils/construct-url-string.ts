fd65b1e9a53e84f3fd19213d57098c2034512dd5
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-09 10:38:46 -0500

    chore: Make path optional in construct URL string util

--- src/utils/construct-url-string.ts
+++ src/utils/construct-url-string.ts
@@ -1,9 +1,9 @@
 export const constructUrlString = (
 	url: string,
-	path: string,
 	params: Record<string, string | number | boolean>,
+	path?: string,
 ): string => {
-	const urlObject = new URL(path, url);
+	const urlObject = path ? new URL(path, url) : new URL(url);
 
 	for (const [key, value] of Object.entries(params)) {
 		urlObject.searchParams.set(key, value.toString());
