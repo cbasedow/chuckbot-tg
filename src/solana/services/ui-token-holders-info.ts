e03f66df3a0b46d7405a0b44043d5c067931189d
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-09 13:40:36 -0500

    feat: Add ui token holders info service
    - Add UI token holders info fetch function
    - Fetches the token holders account info from solana-rpc method
    - Extracts the UI info from the array of accounts info
    - Log errors
    - Add JSDoc description

---
+++ src/solana/services/ui-token-holders-info.ts
@@ -0,0 +1,44 @@
+import { logger } from "$/utils/logger";
+import type BigNumber from "bignumber.js";
+import type { ResultAsync } from "neverthrow";
+import type { UITokenHoldersInfo } from "../schemas/ui-token-holders-info";
+import { extractUITokenHoldersInfo } from "../transformers/token-holders-account-info";
+import { fetchTokenHoldersAccountsInfo } from "./solana-rpc";
+
+type FetchUITokenHoldersInfoParams = {
+	tokenAddress: string;
+	devAddress: string;
+	bondingCurveAddress: string | null;
+	circulatingSupplyBn: BigNumber;
+};
+
+/**
+ * Fetches UI token holders info from Solana RPC
+ * @param tokenAddress - Base58 encoded Solana address
+ * @param devAddress - Base58 encoded Solana address
+ * @param bondingCurveAddress - Base58 encoded Solana address
+ * @param circulatingSupplyBn - BigNumber
+ */
+export const fetchUITokenHoldersInfo = ({
+	tokenAddress,
+	devAddress,
+	bondingCurveAddress,
+	circulatingSupplyBn,
+}: FetchUITokenHoldersInfoParams): ResultAsync<UITokenHoldersInfo, Error> => {
+	return fetchTokenHoldersAccountsInfo(tokenAddress)
+		.andThen((holdersAccountInfo) => {
+			return extractUITokenHoldersInfo({
+				holdersAccountInfo,
+				devAddress,
+				bondingCurveAddress,
+				circulatingSupplyBn,
+			});
+		})
+		.mapErr((error) => {
+			const finalError = new Error(
+				`Failed to fetch UI token holders info for token address ${tokenAddress}: ${error.message}`,
+			);
+			logger.error(finalError);
+			return finalError;
+		});
+};
