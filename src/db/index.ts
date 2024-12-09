68d3542ede2843515ec9d06d5ebc635f48a881f6
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-09 15:17:57 -0500

    feat: Add database connection with drizzle
    - Add postgres client
    - Add db export with drizzle and postgres

---
+++ src/db/index.ts
@@ -0,0 +1,10 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import { envConfig } from "env";
+import postgres from "postgres";
+
+const client = postgres(envConfig.DATABASE_URL, {
+	max: 15,
+	idle_timeout: 30, // seconds
+});
+
+export const db = drizzle({ client });
