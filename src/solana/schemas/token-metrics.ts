b9456311abb9e04f5d7b89d9d5d37732b500a733
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-09 11:45:16 -0500

    feat: Add token metrics schema
    - Add default metrics schema and type
    - Add dexscreener metrics schema and type
    - Add moonshot metrics schema and type
    - Add pumpfun metrics schema and type
    - Add main token metrics schema and type

---
+++ src/solana/schemas/token-metrics.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { bigNumberSchema } from "./bignumber";
+import { solanaBase58AddressSchema } from "./solana";
+
+export const defaultTokenMetricsSchema = z.object({
+	poolName: z.string().min(1),
+	baseTokenAddress: solanaBase58AddressSchema,
+	priceSolBn: bigNumberSchema,
+	priceUsdBn: bigNumberSchema,
+	mcapUsdBn: bigNumberSchema,
+	liquidityUsdBn: bigNumberSchema,
+});
+
+export const dexscreenerTokenMetricsSchema = defaultTokenMetricsSchema.extend({
+	quoteTokenAddress: solanaBase58AddressSchema,
+	poolAddress: solanaBase58AddressSchema,
+	volume1hUsdBn: bigNumberSchema,
+	totalBuys1h: z.number().min(0),
+	totalSells1h: z.number().min(0),
+	priceChange1hPercent: z.number().min(-100).max(100),
+});
+
+export const moonshotTokenMetricsSchema = dexscreenerTokenMetricsSchema.extend({
+	poolName: z.literal("moonshot"),
+	liquidityUsdBn: z.literal(null),
+	bondingProgress: z.number().min(0).max(100).nullable(),
+});
+
+export const pumpfunTokenMetricsSchema = defaultTokenMetricsSchema.extend({
+	poolName: z.literal("pumpfun"),
+	reachedKothAt: z.number().min(0).nullable(),
+	liquidityUsdBn: bigNumberSchema,
+});
+
+export const tokenMetricsSchema = z.union([
+	dexscreenerTokenMetricsSchema,
+	moonshotTokenMetricsSchema,
+	pumpfunTokenMetricsSchema,
+]);
+
+export type DexScreenerTokenMetrics = z.infer<typeof dexscreenerTokenMetricsSchema>;
+export type MoonshotTokenMetrics = z.infer<typeof moonshotTokenMetricsSchema>;
+export type PumpFunTokenMetrics = z.infer<typeof pumpfunTokenMetricsSchema>;
+export type TokenMetrics = z.infer<typeof tokenMetricsSchema>;
