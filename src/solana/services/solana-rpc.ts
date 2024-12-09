8ea2339a0a94777e8e137f96e969d7fa265abc1f
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-09 13:00:21 -0500

    feat: Add solana rpc service
    - Add token holders accounts info fetch function
    - Error handling for empty accounts array
    - Map through the accounts to only return the parsed account info

---
+++ src/solana/services/solana-rpc.ts
@@ -0,0 +1,47 @@
+import { enhancedFetch } from "$/utils/enhanced-fetch";
+import { handleEnhancedResponse } from "$/utils/handle-enhanced-response";
+import { type ResultAsync, errAsync, okAsync } from "neverthrow";
+import { HELIUS_RPC_BASE_REQUEST, HELIUS_RPC_URL, SOLANA_PROGRAM_IDS } from "../constants";
+import { type ParsedTokenAccountInfo, parsedProgramAccountsResponseSchema } from "../schemas/solana";
+
+export const fetchTokenHoldersAccountsInfo = (tokenAddress: string): ResultAsync<ParsedTokenAccountInfo[], Error> => {
+	return enhancedFetch(HELIUS_RPC_URL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			...HELIUS_RPC_BASE_REQUEST,
+			method: "getProgramAccounts",
+			params: [
+				SOLANA_PROGRAM_IDS.SPL_TOKEN,
+				{
+					encoding: "jsonParsed",
+					filters: [
+						{
+							dataSize: 165,
+						},
+						{
+							memcmp: {
+								offset: 0,
+								bytes: tokenAddress,
+							},
+						},
+					],
+				},
+			],
+		}),
+	})
+		.andThen((response) =>
+			handleEnhancedResponse(response, parsedProgramAccountsResponseSchema, "fetchTokenHolderAccountsInfo"),
+		)
+		.map((parsedData) => parsedData.result)
+		.andThen((parsedAccounts) => {
+			if (parsedAccounts.length === 0) {
+				return errAsync(new Error("No token holders accounts found"));
+			}
+
+			// Only return the parsed token account info from each account
+			return okAsync(parsedAccounts.map((parsedAccount) => parsedAccount.account.data.parsed.info));
+		});
+};
