e90877c19f61e038ba5204a17ffa8c89c5eb7c1c
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-10 14:48:50 -0500

    feat: Add upsert ref link by platform and group id
    - Validates input data with zod
    - Attempts to insert ref link
    - On conflict, update the url and updatedBy

---
+++ src/db/upserts/upsert-ref-link-by-platform-and-group-id.ts
@@ -0,0 +1,30 @@
+import { ensureError } from "$/utils/ensure-error";
+import { validateZodSchema } from "$/utils/validate-zod-schema";
+import { ResultAsync, errAsync } from "neverthrow";
+import { db } from "..";
+import { type NewDBRefLink, insertRefLinkSchema, refLinks } from "../schema";
+
+export const upsertRefLinkByPlatformAndGroupId = (newRefLink: NewDBRefLink): ResultAsync<void, Error> => {
+	const parsedInputResult = validateZodSchema(newRefLink, insertRefLinkSchema, "NewDBRefLink");
+
+	if (parsedInputResult.isErr()) {
+		return errAsync(parsedInputResult.error);
+	}
+
+	const validatedRefLink = parsedInputResult.value;
+
+	return ResultAsync.fromPromise(
+		db
+			.insert(refLinks)
+			.values(validatedRefLink)
+			.onConflictDoUpdate({
+				target: [refLinks.platform, refLinks.groupId],
+				set: {
+					url: newRefLink.url,
+					updatedBy: newRefLink.updatedBy,
+				},
+			})
+			.then(() => void 0),
+		ensureError,
+	);
+};
