2b73c8acbbeb501dc3733e3fd2e5e23188f8e313
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-10 15:48:28 -0500

    feat: Add format formatted strings bot util
    - Formats an array of parse-mode formatted strings into one

---
+++ src/bot/utils/format-formatted-strings.ts
@@ -0,0 +1,9 @@
+import { type FormattedString, fmt } from "@grammyjs/parse-mode";
+
+/**
+ * Formats an array of grammy parse-mode formatted strings into a single formatted string
+ * @param An array of grammy parse-mode formatted strings
+ */
+export const formatFormattedStrings = (formattedStrings: FormattedString[]) => {
+	return formattedStrings.reduce((acc, curr) => fmt`${acc}${curr}`);
+};
