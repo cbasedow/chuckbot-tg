a527e4b2e57d78824476df88cf2cecef0edb188d
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-11 12:17:47 -0500

    feat: Add Telegram bot emojis constant
    - Add Telegram bot emojis used in reply messaging

---
+++ src/bot/constants.ts
@@ -0,0 +1,12 @@
+export const EMOJIS = {
+	ARROW_LEFT: "⬅️",
+	ARROW_RIGHT: "➡️",
+	ARROW_UP: "⬆️",
+	ARROW_DOWN: "⬇️",
+	RED_X: "❌",
+	GREEN_CHECK: "✅",
+	RED_QUESTION: "❓",
+	LINK: "🔗",
+	POINT_DOWN: "👇",
+	STOP_SIGN: "🛑",
+};
