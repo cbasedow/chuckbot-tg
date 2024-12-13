d87f990faaaa5198a3a5c0b8b4a81add5a6b5f18
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-13 02:28:10 -0500

    feat: Register commands, menus and manage ref conversation
    - Register admins commands
    - Register manage ref links menu
    - Register ref link platforms menu
    - Register manage ref conversation

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,13 +1,19 @@
+import type { DBRefLinkPlatform } from "$/db/schema";
 import { logger } from "$/utils/logger";
 import { autoRetry } from "@grammyjs/auto-retry";
 import { commands } from "@grammyjs/commands";
-import { conversations } from "@grammyjs/conversations";
+import { conversations, createConversation } from "@grammyjs/conversations";
 import { hydrateReply } from "@grammyjs/parse-mode";
 import { envConfig } from "env";
 import { Bot, GrammyError, HttpError, webhookCallback } from "grammy";
 import { Hono } from "hono";
+import { adminCommands } from "./commands/admin";
+import { manageRef } from "./conversations/manage-ref";
+import { createCurrRefLinkDetailsMenu, manageRefLinksMenu, refLinkPlatformsMenu } from "./menus/manage-ref-links";
 import { ensureDBData } from "./middleware/ensure-db-data";
 import type { MyContext } from "./types";
+
+const REF_PLATFORMS: DBRefLinkPlatform[] = ["bonk", "bullx", "maestro", "photon", "shuriken", "trojan"];
 
 const bot = new Bot<MyContext>(envConfig.BOT_TOKEN);
 const app = new Hono();
@@ -29,6 +35,19 @@ bot.api.config.use(
 		maxDelaySeconds: 5, // Max delay of 5 seconds
 	}),
 );
+
+// Conversations
+bot.use(createConversation(manageRef, "manage-ref"));
+
+// Menus
+for (const platform of REF_PLATFORMS) {
+	refLinkPlatformsMenu.register(createCurrRefLinkDetailsMenu(platform));
+}
+manageRefLinksMenu.register(refLinkPlatformsMenu);
+bot.use(manageRefLinksMenu);
+
+// Commands
+bot.use(adminCommands);
 
 // Long polling in development
 if (envConfig.NODE_ENV === "development") {
