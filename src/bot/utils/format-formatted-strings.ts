3f35446ac7f6c4cab32429537693dd479e4da035
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-13 02:17:20 -0500

    fix: Fix JSDoc param in formatFormattedStrings util

--- src/bot/utils/format-formatted-strings.ts
+++ src/bot/utils/format-formatted-strings.ts
@@ -2,7 +2,7 @@ import { type FormattedString, fmt } from "@grammyjs/parse-mode";
 
 /**
  * Formats an array of grammy parse-mode formatted strings into a single formatted string
- * @param An array of grammy parse-mode formatted strings
+ * @param - An array of grammy parse-mode formatted strings
  */
 export const formatFormattedStrings = (formattedStrings: FormattedString[]) => {
 	return formattedStrings.reduce((acc, curr) => fmt`${acc}${curr}`);
