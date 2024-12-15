9536fee38a585c34765bd06c547d9df244e499f4
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-15 15:27:06 -0500

    feat: Register group address listener menu in bot config

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -10,6 +10,7 @@ import { adminCommands } from "./commands/admin";
 import { userCommands } from "./commands/user";
 import { REF_PLATFORMS } from "./constants";
 import { manageRef } from "./conversations/manage-ref";
+import { groupAddressListenerMenu } from "./menus/group-address-listener";
 import { createCurrRefLinkDetailsMenu, manageRefLinksMenu, refLinkPlatformsMenu } from "./menus/manage-ref-links";
 import { ensureDBData } from "./middleware/ensure-db-data";
 import type { MyContext } from "./types";
@@ -44,6 +45,7 @@ for (const platform of REF_PLATFORMS) {
 }
 manageRefLinksMenu.register(refLinkPlatformsMenu);
 bot.use(manageRefLinksMenu);
+bot.use(groupAddressListenerMenu);
 
 // Commands
 bot.use(adminCommands);
