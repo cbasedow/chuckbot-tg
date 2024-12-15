58a2da50e5b61d7e7eb3f63c1c18b45e7eb9ce6f
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-15 12:15:35 -0500

    feat: Add update group address listener update function

---
+++ src/db/updates/update-group-address-listener.ts
@@ -0,0 +1,35 @@
+import { groups } from "$/db/schema";
+import { ensureError } from "$/utils/ensure-error";
+import { validateZodSchema } from "$/utils/validate-zod-schema";
+import { eq } from "drizzle-orm";
+import { ResultAsync, errAsync } from "neverthrow";
+import { z } from "zod";
+import { db } from "..";
+
+const updateGroupAddressListenerSchema = z.object({
+	groupId: z.number().min(1),
+	isListenerEnabled: z.boolean(),
+});
+
+export const updatedGroupAddressListener = (groupId: number, isListenerEnabled: boolean): ResultAsync<void, Error> => {
+	const parsedInputResult = validateZodSchema(
+		{ groupId, isListenerEnabled },
+		updateGroupAddressListenerSchema,
+		"updateGroupAddressListener",
+	);
+
+	if (parsedInputResult.isErr()) {
+		return errAsync(parsedInputResult.error);
+	}
+	const { groupId: validatedGroupId, isListenerEnabled: validatedIsListenerEnabled } = parsedInputResult.value;
+	return ResultAsync.fromPromise(
+		db
+			.update(groups)
+			.set({
+				addressListenerEnabled: validatedIsListenerEnabled,
+			})
+			.where(eq(groups.id, validatedGroupId))
+			.then(() => void 0),
+		ensureError,
+	);
+};
