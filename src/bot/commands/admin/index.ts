1dc0c17058f1059455d874675565ccccbce7c9c6
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-15 15:26:25 -0500

    feat: Add listener command to admin commands group

--- src/bot/commands/admin/index.ts
+++ src/bot/commands/admin/index.ts
@@ -1,5 +1,6 @@
 import type { MyContext } from "$/bot/types";
 import { CommandGroup } from "@grammyjs/commands";
+import { listenerCommand } from "./listener";
 import { refsCommand } from "./refs";
 
-export const adminCommands = new CommandGroup<MyContext>().add([refsCommand]);
+export const adminCommands = new CommandGroup<MyContext>().add([listenerCommand, refsCommand]);
