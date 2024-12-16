03c6b145bbd98aa81632b3d4ee0b92d0f45a604f
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-16 12:03:48 -0500

    feat: Add only group admin middleware to refs command

--- src/bot/commands/admin/refs.ts
+++ src/bot/commands/admin/refs.ts
@@ -1,5 +1,6 @@
 import { EMOJIS, REF_PLATFORMS } from "$/bot/constants";
 import { manageRefLinksMenu } from "$/bot/menus/manage-ref-links";
+import { onlyGroupAdmin } from "$/bot/middleware/only-group-admin";
 import type { MyContext } from "$/bot/types";
 import { formatFormattedStrings } from "$/bot/utils/format-formatted-strings";
 import type { DBGroupWithRefLinks } from "$/db/schema";
@@ -7,26 +8,29 @@ import { capitalizeFirstLetter } from "$/utils/capitalize-first-letter";
 import { Command } from "@grammyjs/commands";
 import { type FormattedString, bold, code, fmt, italic } from "@grammyjs/parse-mode";
 
-export const refsCommand = new Command<MyContext>("refs", "View and manage group ref links", async (ctx) => {
-	const messageId = ctx.msgId;
-	const dbData = ctx.dbData;
+export const refsCommand = new Command<MyContext>("refs", "View and manage group ref links", [
+	onlyGroupAdmin("Only group admins can view and manage group ref links"),
+	async (ctx) => {
+		const messageId = ctx.msgId;
+		const dbData = ctx.dbData;
 
-	if (!(dbData.group && dbData.user)) {
-		return await ctx.replyFmt(
-			fmt`${EMOJIS.RED_X} ${bold("An error occured getting group and user data. Please try again later")}`,
-		);
-	}
+		if (!(dbData.group && dbData.user)) {
+			return await ctx.replyFmt(
+				fmt`${EMOJIS.RED_X} ${bold("An error occured getting group and user data. Please try again later")}`,
+			);
+		}
 
-	await ctx.replyFmt(formatRefLinksMessage(dbData.group), {
-		link_preview_options: {
-			is_disabled: true,
-		},
-		reply_markup: manageRefLinksMenu,
-		reply_parameters: {
-			message_id: messageId,
-		},
-	});
-}).addToScope({
+		await ctx.replyFmt(formatRefLinksMessage(dbData.group), {
+			link_preview_options: {
+				is_disabled: true,
+			},
+			reply_markup: manageRefLinksMenu,
+			reply_parameters: {
+				message_id: messageId,
+			},
+		});
+	},
+]).addToScope({
 	type: "all_chat_administrators",
 });
 
