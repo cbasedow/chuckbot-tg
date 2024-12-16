620f6585ac082b57038937f2dade5f8dbe3707aa
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-15 22:54:07 -0500

    feat: Add feedback command to user commands group

--- src/bot/commands/user/index.ts
+++ src/bot/commands/user/index.ts
@@ -1,7 +1,13 @@
 import type { MyContext } from "$/bot/types";
 import { CommandGroup } from "@grammyjs/commands";
 import { listenerCommand } from "../shared/listener";
+import { feedbackCommand } from "./feedback";
 import { helpCommand } from "./help";
 import { scanCommand } from "./scan";
 
-export const userCommands = new CommandGroup<MyContext>().add([helpCommand, listenerCommand, scanCommand]);
+export const userCommands = new CommandGroup<MyContext>().add([
+	feedbackCommand,
+	helpCommand,
+	listenerCommand,
+	scanCommand,
+]);
