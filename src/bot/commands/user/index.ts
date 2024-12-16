05e119e643853de7dce390f688f54e588a28513f
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-16 12:29:49 -0500

    feat: Add start command to user commands group

--- src/bot/commands/user/index.ts
+++ src/bot/commands/user/index.ts
@@ -4,10 +4,12 @@ import { listenerCommand } from "../shared/listener";
 import { feedbackCommand } from "./feedback";
 import { helpCommand } from "./help";
 import { scanCommand } from "./scan";
+import { startCommand } from "./start";
 
 export const userCommands = new CommandGroup<MyContext>().add([
 	feedbackCommand,
 	helpCommand,
 	listenerCommand,
 	scanCommand,
+	startCommand,
 ]);
