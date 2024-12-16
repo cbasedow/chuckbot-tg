590ae7185f3cbb68e5b94dae2ce9cf3000fa8837
Charles J Basedow <charlesjbasedow@proton.me>
2024-12-16 12:03:29 -0500

    feat: Add only group admin custom middleware
    - Custom grammY middleware for only group admins

---
+++ src/bot/middleware/only-group-admin.ts
@@ -0,0 +1,42 @@
+import { logger } from "$/utils/logger";
+import { bold, fmt } from "@grammyjs/parse-mode";
+import type { NextFunction } from "grammy";
+import { EMOJIS } from "../constants";
+import type { MyContext } from "../types";
+
+export const onlyGroupAdmin = (customMessage?: string) => {
+	return async (ctx: MyContext, next: NextFunction) => {
+		// Early return if the context is not available
+		if (!(ctx.chat && ctx.from?.id)) {
+			logger.debug("No chat or user context available in OnlyGroupAdmin middleware");
+			return;
+		}
+
+		const chatType = ctx.chat.type;
+
+		// Allow in channels and private chats
+		if (chatType === "channel" || chatType === "private") {
+			return await next();
+		}
+
+		// Allow anonymous admin bots
+		if (ctx.from?.username === "GroupAnonymousBot") {
+			return await next();
+		}
+
+		try {
+			const chatMember = await ctx.getChatMember(ctx.from.id);
+
+			if (chatMember.status === "administrator" || chatMember.status === "creator") {
+				return await next();
+			}
+
+			// Non admin reply message
+			const message = customMessage ?? "Only group admins can use this command";
+			return await ctx.replyFmt(fmt`${EMOJIS.STOP_SIGN} ${bold(message)}`);
+		} catch (error) {
+			logger.error(`Failed to get chat member for user ${ctx.from.id} in OnlyGroupAdmin middleware: ${error}`);
+			return;
+		}
+	};
+};
